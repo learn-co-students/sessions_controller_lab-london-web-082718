9282ef5dffecfe8ce75fcde6777a079a5efb3a22
klbennett <https://github.com/settings/applications>
2018-10-01 18:05:12 +0100

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def index
+    @name = session[:name]
+  end
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+
+end
